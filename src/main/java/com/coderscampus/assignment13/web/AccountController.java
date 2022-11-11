2293a62585580316e7a6f1ea36c7d183baa8a8e7
Robert Gleason <l2ob122@gmail.com>
2022-11-11 10:34:53 -0800

    added a Account Controller, and created an accounts html page

--- src/main/java/com/coderscampus/assignment13/web/AccountController.java
+++ src/main/java/com/coderscampus/assignment13/web/AccountController.java
@@ -1,8 +1,31 @@
 package com.coderscampus.assignment13.web;
 
+import com.coderscampus.assignment13.domain.Account;
+import com.coderscampus.assignment13.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AccountController {
+
+    @Autowired
+    private AccountService accountService;
+
+    // create get mapping for accounts
+    @GetMapping("/accounts")
+    public String getAccounts(ModelMap model) {
+        model.put("accounts", new Account());
+        return "accounts";
+    }
+
+    // create post mapping for accounts
+    @PostMapping("/accounts")
+    public String postAccounts(Account account) {
+        accountService.save(account);
+        return "redirect:/accounts";
+    }
 
 }
